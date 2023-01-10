58c550aed2e31b595a5af5a73889202a70db03e9
Stefan Miller <stefanmiller739@gmail.com>
2023-01-10 18:23:21 -0500

    added component html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import { Button } from './components/button';
+import { Counter } from './components/counter';
+
 function App() {
   return (
     <div className="App">
-      
+      <Button />
+      <Counter />
     </div>
   );
 }
---
+++ src/components/counter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Counter = () => {
+  return (
+    <div>The button has been clicked x times.</div>
+  )
+}
