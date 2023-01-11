79ecb573ac408c50e4ecd39a1cec0ef34ec6d519
Stefan Miller <stefanmiller739@gmail.com>
2023-01-10 19:13:24 -0500

    testing the api call via button click

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 export const Button = () => {
+  
+  const btnClick = () => {
+    fetch('https://api.countapi.xyz/hit/1ccb732e-b55a-4404-ad3f-0f99c02fe44e/smclicks') 
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    console.log("clicked");
+  }
+
   return (
     <div>
-        <button>Click Me</button>
+        <button onClick={btnClick}>Click Me</button>
     </div>
   )
 }
